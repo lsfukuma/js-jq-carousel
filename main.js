a6057b92d6316815d69cab77070d017eab40837a
Leticia <lsf@protonmail.ch>
2020-04-30 17:24:53 +0200

    non torna alla prima img

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 //slider
 $('i.fas.fa-chevron-right').click(function(){
-    //trouver l'element
-    var currentImg = $('img.active');
-    //
+    //trouver l'element  qui a la classe active
+    var currentImg = $('.active');
+    //creare una variabile per l'img successiva
+    var nextImg = currentImg.next()
+    if (nextImg.length != 0) {
+        currentImg.removeClass('active');
+        nextImg.addClass('active');
+    } else {
+        currentImg = ('img.first-child')
+        console.log('currentImg');
+    }
+});
+
+
+$('i.fas.fa-chevron-left').click(function(){
+    //trouver l'element  qui a la classe active
+    var currentImg = $('.active');
+    //creare una variabile per l'img successiva
+    var prevImg = currentImg.prev()
+    if (prevImg.length != 0) {
+        currentImg.removeClass('active');
+        prevImg.addClass('active');
+    } else {
+        currentImg = ('img.first-child')
+    }
 });
