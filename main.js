93ae02d8c8b30e523305ba3762536fa021bf3075
Leticia <lsf@protonmail.ch>
2020-05-01 17:47:47 +0200

    palline inizio

--- main.js
+++ main.js
@@ -2,14 +2,21 @@
 $('i.fas.fa-chevron-right').click(function(){
     //trouver l'element  qui a la classe active
     var currentImg = $('.active');
+    //pallina attiva
+    var currentDot = $('.active');
     //creare una variabile per l'img successiva per poter creare le condizioni
     var nextImg = currentImg.next();
-    var firstImg = $('img:first-child');
+    //pallina successiva
+    var nextDot = currentDot.next();
+    //var firstImg = $('img:first-child');
     if (nextImg.length != 0) {
         currentImg.removeClass('active');
         nextImg.addClass('active');
+        currentDot.removeClass('active');
+        nextDot.addClass('active')
     } else {
         $('img:first-child').addClass('active');
+        $('img:last-child').removeClass('active');
     }
 });
 
@@ -24,6 +31,16 @@ $('i.fas.fa-chevron-left').click(function(){
         prevImg.addClass('active');
     } else {
         $('img:last-child').addClass('active');
+        $('img:first-child').removeClass('active');
 
     }
 });
+//esecuto la funzione delle palline
+$('i.fas.fa-circle').click(palline);
+
+//mettere le palline e fare il js
+function palline() {
+    //creare una variabile per il valore della posizione dell'img
+    var imgPosition = $('i.circle').index();
+    console.log(imgPosition);
+}
