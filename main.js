f2e903666549af79fec585886b6eb4dc72847640
Leticia <lsf@protonmail.ch>
2020-05-05 11:37:23 +0200

    bonus -

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ $('i.fas.fa-chevron-right').click(function(){
     }
 });
 
-//freccia <
+//previous
 $('i.fas.fa-chevron-left').click(function(){
     //trouver l'element  qui a la classe active
     var currentImg = $('.active');
@@ -46,31 +46,46 @@ $('i.fas.fa-chevron-left').click(function(){
 
     }
 });
-//bonus esecuto la funzione delle palline
-//$('i.fas.fa-circle').click(palline);
 
-//mettere le palline e fare il js
-// var imgPosition = 0;
-// function palline() {
-//     //creare una variabile per il valore della posizione dell'img
-//     var imgPosition = 0;
-//     imgPosition = $('i.dots').index();
-//     //nascondere tutte img
-//     console.log(imgPosition);
-//     $('img.wrapper').hide();
-//     //far vedere l'img corrente
-//     $('img.wrapper:nth-child('+ imgPosition +')').fadeIn();
-// }
+//bonus: quando l'utente clicca su uno pallino, devo mostrare l'img
+$('i.fas.fa-circle').click(function(){
+    //lavorare con index e farsi che sia collegato all img con lo stesso index
+    //img corrente
+    var currentImg = $('img.active');
+    //pallina attiva
+    var currentDot = $('i.fas.fa-circle.active');
+    //rimuovo la classe dell'img corrente
+    currentImg.removeClass('active');
+    //rimuovo la classe del pallino corrente
+    currentDot.removeClass('active');
+    //aggiungo la classe active al pallino su cui l'utente ha cliccato
+    $(this).addClass('active');
+    //vedere la posizione del pallino
+    var dotPosition = $(this).index();
+    //recupero l'img con la stessa posiwione del pallino
+    var imgCorrispondente = $('img.wrapper').eq(imgCorrispondente);
+});
 
-var imgPosition = $('img.active').index();
-var currentImg = $('img.active');
-var nextImg = currentImg.next();
-var currentSlide = 1;
-//creare un autoplay --> setInterval
 var autoplay = setInterval(function(){
-    
-    currentSlide++
-    if (imgPosition = 0) {
-        imgPosition = 1;
-    // }
+    var currentImg = $('img.active');
+    //pallina attiva
+    var currentDot = $('i.fas.fa-circle.active');
+    //rimuovo la classe dell'img corrente
+    currentImg.removeClass('active');
+    //rimuovo la classe del pallino corrente
+    currentDot.removeClass('active');
+    //creare una variabile per l'img successiva per poter creare le condizioni
+    var nextImg = currentImg.next();
+    //pallina successiva
+    var nextDot = currentDot.next();
+    if (nextImg.length != 0) {
+        //se esiste un img successivo (length diverso da 0) allora si aggiunge la classe active all img successiva
+        nextImg.addClass('active');
+        //aggiungo la classe active al pallino successivo
+        nextDot.addClass('active');
+    } else {
+        $('img:first-child').addClass('active');
+        $('img:last-child').removeClass('active');
+    }
+
 }, 1000);
